a2c64c0bbb9cbcdee904aa35fc9a65c06f1bb85a
JRichm <jblackenmusic@gmail.com>
2026-02-26 23:43:37 -0700

    corrected definition of protocol class

--- M8_P1_dessert_shop_6_packable/packable.py
+++ M8_P1_dessert_shop_6_packable/packable.py
@@ -1,13 +1,13 @@
-class Packable:
-    def __init__(self, packaging: str):
-        self._packaging = packaging
+from typing import Protocol
 
-    
+
+class Packable:
+
     @property
     def packaging(self) -> str:
-        return self._packaging
+        ...
     
 
     @packaging.setter
     def packaging(self, new_packaging: str):
-        self._packaging = new_packaging
+        ...
